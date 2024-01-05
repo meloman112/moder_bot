b352b81b18cc11f22336174e63f098a9959a35cf
meloman112 <61692989+meloman112@users.noreply.github.com>
2024-01-06 01:29:28 +0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,42 @@
+from aiogram import Bot, Dispatcher, types
+from aiogram.utils import executor
+import motor.motor_asyncio as mt
+from datetime import datetime
+
+API_TOKEN = '6126126956:AAF1CNSusKfw9xp6XGBy48vXAFQhPnkW1qM'
+
+bot = Bot(token=API_TOKEN)
+dp = Dispatcher(bot)
+
+
+clinet = mt.AsyncIOMotorClient('mongodb+srv://meloman:paraxatov11@cluster0.eqlglqy.mongodb.net/DB_test?retryWrites=true&w=majority')
+collection = clinet.DB_Moder.Collection_Moder_bot
+
+
+async def add_user(user_id, name):
+    date = datetime.now().date()
+    collection.insert_one({
+        '_id': user_id,
+        'name': name,
+        'date': str(date)
+    })
+
+
+
+@dp.message_handler(content_types=types.ContentType.NEW_CHAT_MEMBERS)
+async def delete_join_message(message: types.Message):
+    await message.delete()
+
+@dp.message_handler(content_types=types.ContentType.LEFT_CHAT_MEMBER)
+async def delete_left_message(message: types.Message):
+    await message.delete()
+
+@dp.message_handler(commands=['start'])
+async def send_welcome(message: types.Message):
+    name = message.from_user.first_name
+    await message.reply(f"Привет {name}! Я бот, который удаляет уведомления о вступлении и выходе из группы, а также о закреплении сообщений. Добавь меня в группу и предоставь права админа")
+    user_id = message.from_user.id
+    await add_user(user_id, name)
+
+if __name__ == '__main__':
+    executor.start_polling(dp, skip_updates=True)
